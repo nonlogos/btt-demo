5cf7154264f7b0ec15e0639590f326c545309375
nonlogos <nonlogos1@gmail.com>
2023-03-26 19:10:11 -0500

    fixed preEnrolling spacing

--- components/PreEnrolling.tsx
+++ components/PreEnrolling.tsx
@@ -6,15 +6,16 @@ import adultsProgramImage from '../../public/group-kids.jpg';
 const Container = styled(Box)`
 	position: relative;
 	gap: var(--sp-4x);
-	margin-top: 2rem;
+	margin-top: 3rem;
 	height: 30rem;
 	padding: var(--sp-2x);
 	text-align: center;
 	background: linear-gradient(to bottom, white 5%, #7b929c 98%), url('/group-kids.jpg');
 	background-blend-mode: hard-light;
-	background-position: 38% -5%;
+	background-position: 48% -5%;
 	background-repeat: no-repeat;
 	background-size: cover;
+	border-top: 2px solid white; // to hide the bordeer of the image
 	@media (min-width: 707px) {
 		margin-top: 8rem;
 	}
