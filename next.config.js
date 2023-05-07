4435085b148412f75be5a16e8c1f5c3159135b27
nonlogos <nonlogos1@gmail.com>
2023-05-07 13:52:41 -0500

    add github pages deploy config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,27 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '/';
+
+if (isGithubActions) {
+	// trim off `<owner>/`
+	const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+
+	assetPrefix = `/${repo}/`;
+	basePath = `/${repo}`;
+}
 const nextConfig = {
+	assetPrefix: assetPrefix,
+	basePath: basePath,
 	reactStrictMode: false,
 	swcMinify: true,
 	compiler: {
 		styledComponents: true,
+	},
+	images: {
+		unoptimized: true,
 	},
 	webpack: (config, { isServer }) => {
 		const prefix = config.assetPrefix ?? config.basePath ?? '';
