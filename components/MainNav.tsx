edbaaaf7db215b30a3e9e0821a72e89871742992
nonlogos <nonlogos1@gmail.com>
2023-05-08 10:45:52 -0500

    change nav

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { Home, Info, List, LocalPhone, PersonAddAlt1 } from '@mui/icons-material';
+import { Home, Info, List, LocalPhone, CalendarToday } from '@mui/icons-material';
 import { ActiveLink } from './ActiveLink';
 import { SIGN_UP_URL } from '../siteContent';
 
@@ -8,7 +8,7 @@ export const mainNavList = [
 	{ url: '/about', name: 'about', icon: Info },
 	{ url: '/programs', name: 'programs', icon: List },
 	{ url: '/contact', name: 'contact', icon: LocalPhone },
-	{ url: SIGN_UP_URL, name: 'sign Up', icon: PersonAddAlt1 },
+	{ url: '/schedules', name: 'schedules', icon: CalendarToday },
 ];
 
 const StyledLi = styled.li`
